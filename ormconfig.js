ecb72d0755be718b7dddf1e17139cf5225daef22
Randal <randal.t.krebs@gmail.com>
2024-01-29 18:58:56 -0300

    feat(config): switch to use environment variables

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'randaltk98',
-  database: 'brain-agriculture',
-  synchronize: true,
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: process.env.DB_SYNCHRONIZE,
   entities: ['dist/**/*.entity.js'],
 };
+
