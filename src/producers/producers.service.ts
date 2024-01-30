117a88e2d9b72612105f1363621d6bb54e0f7f20
Randal <randal.t.krebs@gmail.com>
2024-01-30 09:53:26 -0300

    feat: add sum logic arable area and vegetation must not be greater than the total area

--- src/producers/producers.service.ts
+++ src/producers/producers.service.ts
@@ -15,11 +15,31 @@ export class ProducersService {
     return cpf.isValid(value) || cnpj.isValid(value);
   }
 
+
+
   async createProducer(producer: Producer): Promise<Producer> {
+    const existingProducer = await this.producersRepository.findOne({ where: { cpfCnpj: producer.cpfCnpj } });
+
+    
+    if (existingProducer) {
+      throw new BadRequestException('CPF/CNPJ already registered');
+    }
+
+
     if (!this.validateCPFAndCNPJ(producer.cpfCnpj)) {
       throw new BadRequestException('Invalid CPF or CNPJ');
     }
+  
+    const precision = 2; 
 
+    const roundedAgricultavel = Math.round(producer.areaAgricultavel * 10 ** precision) / 10 ** precision;
+    const roundedVegetacao = Math.round(producer.areaVegetacao * 10 ** precision) / 10 ** precision;
+    const roundedTotal = Math.round(producer.areaTotal * 10 ** precision) / 10 ** precision;
+    
+    if (roundedAgricultavel + roundedVegetacao > roundedTotal) {
+      throw new BadRequestException('Sum of areaAgricultavel and areaVegetacao cannot be greater than areaTotal');
+    }
+  
     return await this.producersRepository.save(producer);
   }
 }
