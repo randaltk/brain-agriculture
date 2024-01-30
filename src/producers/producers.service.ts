7c8210a72840cdddc1cff5ab61470a1926038edd
Randal <randal.t.krebs@gmail.com>
2024-01-30 09:24:42 -0300

    feat: add cpf cnpj validator

--- src/producers/producers.service.ts
+++ src/producers/producers.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Producer } from './producer.entity';
-import { ProducersModule } from './producers.module';
+import { cpf, cnpj } from 'cpf-cnpj-validator';
 
 @Injectable()
 export class ProducersService {
   constructor(
     @InjectRepository(Producer)
     private readonly producersRepository: Repository<Producer>,
-  ) {}
+  ) { }
 
-  async createProducer(producer: Producer): Promise<Producer> {
-    return await this.producersRepository.save(producer);
+  private validateCPFAndCNPJ(value: string): boolean {
+    return cpf.isValid(value) || cnpj.isValid(value);
   }
 
- 
+  async createProducer(producer: Producer): Promise<Producer> {
+    if (!this.validateCPFAndCNPJ(producer.cpfCnpj)) {
+      throw new BadRequestException('Invalid CPF or CNPJ');
+    }
+
+    return await this.producersRepository.save(producer);
+  }
 }
